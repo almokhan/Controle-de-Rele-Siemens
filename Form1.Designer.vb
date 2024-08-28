cd976cb03cf8c1457e20549e9bdc50bac6d45b51
almokhan <118139242+almokhan@users.noreply.github.com>
2024-08-28 14:55:08 -0300

    Add files via upload
    Primeiro e unico commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Conectbtn = New Button()
+        Disconetbtn = New Button()
+        GroupBox1 = New GroupBox()
+        btnFalso = New Button()
+        btnVerdade = New Button()
+        btnM80 = New Button()
+        btnM70 = New Button()
+        btnM60 = New Button()
+        btnM90 = New Button()
+        btnM10 = New Button()
+        btnM11 = New Button()
+        btnM05 = New Button()
+        Label2 = New Label()
+        TextBox3 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        MonitorTimer = New Timer(components)
+        btnLR2 = New Button()
+        btnLR1 = New Button()
+        btnQR1 = New Button()
+        btnQR2 = New Button()
+        btnOff = New Button()
+        GroupBox2 = New GroupBox()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Conectbtn
+        ' 
+        Conectbtn.Location = New Point(14, 12)
+        Conectbtn.Name = "Conectbtn"
+        Conectbtn.Size = New Size(79, 37)
+        Conectbtn.TabIndex = 0
+        Conectbtn.Text = "Conectar"
+        Conectbtn.UseVisualStyleBackColor = True
+        ' 
+        ' Disconetbtn
+        ' 
+        Disconetbtn.Enabled = False
+        Disconetbtn.Location = New Point(122, 12)
+        Disconetbtn.Name = "Disconetbtn"
+        Disconetbtn.Size = New Size(82, 37)
+        Disconetbtn.TabIndex = 1
+        Disconetbtn.Text = "Desconectar"
+        Disconetbtn.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(btnFalso)
+        GroupBox1.Controls.Add(btnVerdade)
+        GroupBox1.Controls.Add(btnM80)
+        GroupBox1.Controls.Add(btnM70)
+        GroupBox1.Controls.Add(btnM60)
+        GroupBox1.Controls.Add(btnM90)
+        GroupBox1.Controls.Add(btnM10)
+        GroupBox1.Controls.Add(btnM11)
+        GroupBox1.Controls.Add(btnM05)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Enabled = False
+        GroupBox1.Location = New Point(43, 183)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(374, 203)
+        GroupBox1.TabIndex = 2
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Enviar / Receber"
+        ' 
+        ' btnFalso
+        ' 
+        btnFalso.Location = New Point(152, 131)
+        btnFalso.Name = "btnFalso"
+        btnFalso.Size = New Size(53, 24)
+        btnFalso.TabIndex = 13
+        btnFalso.Text = "0"
+        btnFalso.UseVisualStyleBackColor = True
+        ' 
+        ' btnVerdade
+        ' 
+        btnVerdade.Location = New Point(79, 131)
+        btnVerdade.Name = "btnVerdade"
+        btnVerdade.Size = New Size(53, 24)
+        btnVerdade.TabIndex = 12
+        btnVerdade.Text = "1"
+        btnVerdade.UseVisualStyleBackColor = True
+        ' 
+        ' btnM80
+        ' 
+        btnM80.Location = New Point(142, 37)
+        btnM80.Name = "btnM80"
+        btnM80.Size = New Size(53, 24)
+        btnM80.TabIndex = 11
+        btnM80.Text = "M.80"
+        btnM80.UseVisualStyleBackColor = True
+        ' 
+        ' btnM70
+        ' 
+        btnM70.Location = New Point(99, 37)
+        btnM70.Name = "btnM70"
+        btnM70.Size = New Size(53, 24)
+        btnM70.TabIndex = 9
+        btnM70.Text = "M7.0"
+        btnM70.UseVisualStyleBackColor = True
+        ' 
+        ' btnM60
+        ' 
+        btnM60.Location = New Point(52, 37)
+        btnM60.Name = "btnM60"
+        btnM60.Size = New Size(53, 24)
+        btnM60.TabIndex = 8
+        btnM60.Text = "M6.0"
+        btnM60.UseVisualStyleBackColor = True
+        ' 
+        ' btnM90
+        ' 
+        btnM90.Location = New Point(191, 37)
+        btnM90.Name = "btnM90"
+        btnM90.Size = New Size(53, 24)
+        btnM90.TabIndex = 9
+        btnM90.Text = "M9.0"
+        btnM90.UseVisualStyleBackColor = True
+        ' 
+        ' btnM10
+        ' 
+        btnM10.Location = New Point(239, 37)
+        btnM10.Name = "btnM10"
+        btnM10.Size = New Size(53, 24)
+        btnM10.TabIndex = 6
+        btnM10.Text = "M10.0"
+        btnM10.UseVisualStyleBackColor = True
+        ' 
+        ' btnM11
+        ' 
+        btnM11.Location = New Point(286, 37)
+        btnM11.Name = "btnM11"
+        btnM11.Size = New Size(53, 24)
+        btnM11.TabIndex = 7
+        btnM11.Text = "M11.0"
+        btnM11.UseVisualStyleBackColor = True
+        ' 
+        ' btnM05
+        ' 
+        btnM05.Location = New Point(6, 37)
+        btnM05.Name = "btnM05"
+        btnM05.Size = New Size(53, 24)
+        btnM05.TabIndex = 5
+        btnM05.Text = "M0.5"
+        btnM05.UseVisualStyleBackColor = True
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(28, 136)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(22, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "PC"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(43, 66)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(169, 23)
+        TextBox3.TabIndex = 3
+        TextBox3.Text = "192.168.0.10"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(14, 69)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(17, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "IP"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(313, 389)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(148, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "Conversadores de PLC INC"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(218, 66)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(48, 23)
+        TextBox4.TabIndex = 6
+        TextBox4.Text = "0"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(272, 66)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(48, 23)
+        TextBox5.TabIndex = 7
+        TextBox5.Text = "1"
+        ' 
+        ' MonitorTimer
+        ' 
+        MonitorTimer.Enabled = True
+        MonitorTimer.Interval = 1
+        ' 
+        ' btnLR2
+        ' 
+        btnLR2.Location = New Point(65, 22)
+        btnLR2.Name = "btnLR2"
+        btnLR2.Size = New Size(53, 24)
+        btnLR2.TabIndex = 8
+        btnLR2.Text = "Rele 2"
+        btnLR2.UseVisualStyleBackColor = True
+        ' 
+        ' btnLR1
+        ' 
+        btnLR1.Location = New Point(6, 22)
+        btnLR1.Name = "btnLR1"
+        btnLR1.Size = New Size(53, 24)
+        btnLR1.TabIndex = 9
+        btnLR1.Text = "Rele 1"
+        btnLR1.UseVisualStyleBackColor = True
+        ' 
+        ' btnQR1
+        ' 
+        btnQR1.Location = New Point(124, 22)
+        btnQR1.Name = "btnQR1"
+        btnQR1.Size = New Size(63, 24)
+        btnQR1.TabIndex = 10
+        btnQR1.Text = "Piscar 1"
+        btnQR1.UseVisualStyleBackColor = True
+        ' 
+        ' btnQR2
+        ' 
+        btnQR2.Location = New Point(193, 22)
+        btnQR2.Name = "btnQR2"
+        btnQR2.Size = New Size(89, 24)
+        btnQR2.TabIndex = 11
+        btnQR2.Text = "Piscar Ambos"
+        btnQR2.UseVisualStyleBackColor = True
+        ' 
+        ' btnOff
+        ' 
+        btnOff.Location = New Point(286, 23)
+        btnOff.Name = "btnOff"
+        btnOff.Size = New Size(75, 23)
+        btnOff.TabIndex = 12
+        btnOff.Text = "Desligar"
+        btnOff.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(btnLR1)
+        GroupBox2.Controls.Add(btnOff)
+        GroupBox2.Controls.Add(btnLR2)
+        GroupBox2.Controls.Add(btnQR1)
+        GroupBox2.Controls.Add(btnQR2)
+        GroupBox2.Enabled = False
+        GroupBox2.Location = New Point(43, 116)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(374, 61)
+        GroupBox2.TabIndex = 13
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "Receitas"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(464, 406)
+        Controls.Add(GroupBox2)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox3)
+        Controls.Add(GroupBox1)
+        Controls.Add(Disconetbtn)
+        Controls.Add(Conectbtn)
+        Name = "Form1"
+        Text = "Conversador"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Conectbtn As Button
+    Friend WithEvents Disconetbtn As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents MonitorTimer As Timer
+    Friend WithEvents btnM11 As Button
+    Friend WithEvents btnM10 As Button
+    Friend WithEvents btnM05 As Button
+    Friend WithEvents btnM60 As Button
+    Friend WithEvents btnM70 As Button
+    Friend WithEvents btnM80 As Button
+    Friend WithEvents btnM90 As Button
+    Friend WithEvents btnFalso As Button
+    Friend WithEvents btnVerdade As Button
+    Friend WithEvents btnLR2 As Button
+    Friend WithEvents btnLR1 As Button
+    Friend WithEvents btnQR1 As Button
+    Friend WithEvents btnQR2 As Button
+    Friend WithEvents btnOff As Button
+    Friend WithEvents GroupBox2 As GroupBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,145 @@
+﻿Imports System.Windows.Forms.VisualStyles
+Imports S7.Net
+
+Public Class Form1
+
+    Function Zerar()
+        plc.Write("M0.5", False)
+        plc.Write("M6.0", False)
+        plc.Write("M7.0", False)
+        plc.Write("M8.0", False)
+        plc.Write("M9.0", False)
+        plc.Write("M10.0", False)
+        plc.Write("M11.0", False)
+        plc.Write("M12.0", False)
+        Return (0)
+    End Function
+    Private plc As S7.Net.Plc ' Variável para armazenar a instância do PLC
+    Private memoriaslc As String
+    Public Shared rack As Integer
+    Public Shared slot As Integer
+
+
+    Private Sub Conectbtn_Click(sender As Object, e As EventArgs) Handles Conectbtn.Click
+        Try
+            rack = Integer.Parse(TextBox4.Text)
+            slot = Integer.Parse(TextBox5.Text)
+            plc = New S7.Net.Plc(CpuType.S71200, TextBox3.Text, rack, slot)
+            plc.Open()
+            If plc.IsConnected Then
+                Conectbtn.Enabled = False
+                Conectbtn.BackColor = Color.Green
+                Disconetbtn.Enabled = True
+                GroupBox1.Enabled = True
+                GroupBox2.Enabled = True
+                TextBox3.Enabled = False
+                TextBox4.Enabled = False
+                TextBox5.Enabled = False
+            Else
+                MessageBox.Show("Não foi possível conectar ao PLC.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Erro ao conectar ao PLC: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Disconetbtn_Click(sender As Object, e As EventArgs) Handles Disconetbtn.Click
+        If plc IsNot Nothing AndAlso plc.IsConnected Then
+            plc.Close()
+        End If
+        Conectbtn.Enabled = True
+        Conectbtn.BackColor = Color.White
+        Disconetbtn.Enabled = False
+        GroupBox1.Enabled = False
+        GroupBox2.Enabled = False
+        TextBox3.Enabled = True
+        TextBox4.Enabled = True
+        TextBox5.Enabled = True
+        btnM05.BackColor = Color.Gray
+        btnM10.BackColor = Color.Gray
+        btnM11.BackColor = Color.Gray
+        btnM70.BackColor = Color.Gray
+        btnM60.BackColor = Color.Gray
+        btnM80.BackColor = Color.Gray
+        btnM90.BackColor = Color.Gray
+    End Sub
+
+    Private Sub MonitorTimer_Tick(sender As Object, e As EventArgs) Handles MonitorTimer.Tick
+        If plc IsNot Nothing AndAlso plc.IsConnected Then
+            Try
+                btnM05.BackColor = If(CBool(plc.Read("M0.5")), Color.Green, Color.Gray)
+                btnM10.BackColor = If(CBool(plc.Read("M10.0")), Color.Green, Color.Gray)
+                btnM11.BackColor = If(CBool(plc.Read("M11.0")), Color.Green, Color.Gray)
+                btnM70.BackColor = If(CBool(plc.Read("M7.0")), Color.Green, Color.Gray)
+                btnM60.BackColor = If(CBool(plc.Read("M6.0")), Color.Green, Color.Gray)
+                btnM80.BackColor = If(CBool(plc.Read("M8.0")), Color.Green, Color.Gray)
+                btnM90.BackColor = If(CBool(plc.Read("M9.0")), Color.Green, Color.Gray)
+            Catch ex As Exception
+                MessageBox.Show("Erro ao ler o valor da memória: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MonitorTimer.Interval = 165
+        MonitorTimer.Start()
+        memoriaslc = "M10.0"
+    End Sub
+
+    Private Sub btnVerdade_Click(sender As Object, e As EventArgs) Handles btnVerdade.Click
+        plc.Write(memoriaslc, True)
+    End Sub
+
+    Private Sub btnFalso_Click(sender As Object, e As EventArgs) Handles btnFalso.Click
+        plc.Write(memoriaslc, False)
+    End Sub
+
+    Private Sub btnM05_Click(sender As Object, e As EventArgs) Handles btnM05.Click
+        memoriaslc = "M0.5"
+    End Sub
+    Private Sub btnM60_Click(sender As Object, e As EventArgs) Handles btnM60.Click
+        memoriaslc = "M6.0"
+    End Sub
+    Private Sub btnM70_Click(sender As Object, e As EventArgs) Handles btnM70.Click
+        memoriaslc = "M7.00"
+    End Sub
+    Private Sub btnM80_Click(sender As Object, e As EventArgs) Handles btnM80.Click
+        memoriaslc = "M8.0"
+    End Sub
+    Private Sub btnM90_Click(sender As Object, e As EventArgs) Handles btnM90.Click
+        memoriaslc = "M9.0"
+    End Sub
+    Private Sub btnM10_Click(sender As Object, e As EventArgs) Handles btnM10.Click
+        memoriaslc = "M10.0"
+    End Sub
+    Private Sub btnM11_Click(sender As Object, e As EventArgs) Handles btnM11.Click
+        memoriaslc = "M11.0"
+    End Sub
+
+    Private Sub btnLR1_Click(sender As Object, e As EventArgs) Handles btnLR1.Click
+        Zerar()
+        plc.Write("M6.0", True)
+        plc.Write("M7.0", True)
+
+    End Sub
+
+    Private Sub btnOff_Click(sender As Object, e As EventArgs) Handles btnOff.Click
+        Zerar()
+    End Sub
+
+    Private Sub btnLR2_Click(sender As Object, e As EventArgs) Handles btnLR2.Click
+        Zerar()
+        plc.Write("M10.0", True)
+    End Sub
+
+    Private Sub btnQR1_Click(sender As Object, e As EventArgs) Handles btnQR1.Click
+        Zerar()
+        plc.Write("M7.0", True)
+    End Sub
+
+    Private Sub btnQR2_Click(sender As Object, e As EventArgs) Handles btnQR2.Click
+        Zerar()
+        plc.Write("M7.0", True)
+        plc.Write("M8.0", True)
+    End Sub
+End Class
